ada8e53718d2344ffc32f96bf164d2ffbfb994d4
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2023-09-26 23:37:39 +0200

    fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -28,11 +28,12 @@ const Home = () => {
     <>
       <h2>Trending today</h2>
       <StyleFilmList type="1">
-        {films.map(({ title, id }) => {
+        {films.map(({ title, id, name }) => {
           return (
             <StyleFilmItem className="gallery-film" key={id}>
               <StyleFilmLink to="/movies/:movieId">
                 <StyleFilmTitle>{title}</StyleFilmTitle>
+                <StyleFilmTitle>{name}</StyleFilmTitle>
               </StyleFilmLink>
             </StyleFilmItem>
           );
